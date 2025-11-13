611b225a597ba7c13d38c3642b58215843dec77f
Mike Schinkel <mike@newclarity.net>
2025-11-13 07:06:28 -0500

    chore: Change type from `string` to `dt.Filepath`.

--- json_file_logger.go
+++ json_file_logger.go
@@ -4,13 +4,15 @@ import (
 	"io"
 	"log/slog"
 	"os"
+
+	"github.com/mikeschinkel/go-dt"
 )
 
 // CreateJSONFileLogger creates a new structured logger that writes to a file. The logger
 // uses JSON format for structured logging.
-func CreateJSONFileLogger(file string) (logger *slog.Logger, err error) {
+func CreateJSONFileLogger(file dt.Filepath) (logger *slog.Logger, err error) {
 	var w io.Writer
-	w, err = os.OpenFile(file, os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModePerm)
+	w, err = file.OpenFile(os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModePerm)
 	if err != nil {
 		goto end
 	}
