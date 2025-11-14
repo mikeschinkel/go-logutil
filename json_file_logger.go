941d1cab1f2a31668cd37f1581dcf5a95e51abbf
Mike Schinkel <mike@newclarity.net>
2025-11-14 01:14:27 -0500

    fix: Add logic to create log dir if missing

--- json_file_logger.go
+++ json_file_logger.go
@@ -1,6 +1,7 @@
 package logutil
 
 import (
+	"errors"
 	"io"
 	"log/slog"
 	"os"
@@ -8,10 +9,33 @@ import (
 	"github.com/mikeschinkel/go-dt"
 )
 
+var ErrDirIsOtherEntryType = errors.New("directory is other entry type")
+
 // CreateJSONFileLogger creates a new structured logger that writes to a file. The logger
 // uses JSON format for structured logging.
 func CreateJSONFileLogger(file dt.Filepath) (logger *slog.Logger, err error) {
 	var w io.Writer
+	var status dt.EntryStatus
+
+	dir := file.Dir()
+	status, err = dir.Status()
+	if err != nil {
+		goto end
+	}
+	switch status {
+	case dt.IsDirEntry:
+		// S'all good, man!
+	case dt.IsMissingEntry:
+		err = dir.MkdirAll(0755)
+	default:
+		err = dt.NewErr(
+			ErrDirIsOtherEntryType,
+			"entry_type", status.String(),
+		)
+	}
+	if err != nil {
+		goto end
+	}
 	w, err = file.OpenFile(os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModePerm)
 	if err != nil {
 		goto end
